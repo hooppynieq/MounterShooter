84d95052babc26ace46041d6d55b93d053a3feaf
hooppynieq <hooppynieq@hotmail.com>
2025-10-20 12:21:41 -0300

    Creating shot

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from code.EnemyShot import EnemyShot
 from code.Entity import Entity
-from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.Const import ENTITY_SPEED, WIN_WIDTH, ENTITY_SHOT_DELAY
 
 
 class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
     def move(self, ):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
 
